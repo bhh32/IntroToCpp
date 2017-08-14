d3dd835fed148222239f38b046a64cab08850c56
s179058 <s179058@SEA-8FD024>
2017-08-14 12:45:58 -0700

    Add project files.

---
+++ 1-HelloAIE/main.cpp
@@ -0,0 +1,109 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	// Prints 'Hello AIE' to the console
+	cout << "Hello AIE" << endl;
+
+	// Skips a line
+	cout << "" << endl;
+
+	// Pauses the system and waits for user input
+	system("pause");
+
+	// Skips a line
+	cout << "" << endl;
+
+	// Prints a list of tracks on their own line
+	cout << "Five Finger Death Punch - House of the Rising Sun" << endl;
+	cout << "Five Finger Death Punch - Nemesis" << endl;
+	cout << "Linkin Park - Crawling" << endl;
+	cout << "Jim Croche - Time in a Bottle" << endl;
+	cout << "Taylor Swift - Bad Blood" << endl;
+	cout << "Taylor Swift - Shake It Off" << endl;
+	cout << "Casper Baby Pants - Butterfly Driving a Truck" << endl;
+
+
+	// Skips a line
+	cout << "" << endl;
+
+	// Pauses the system and waits for user input
+	system("pause");
+
+	// Skips a line
+	cout << "" << endl;
+	
+	// For loop that prints 5 - 0
+	for (int i = 5; i > - 1; --i)
+	{
+		// Prints current value of i to screen
+		cout << i << endl;
+	}
+
+	// Skips a line
+	cout << "" << endl;
+
+	// Pauses the system and waits for user input
+	system("pause");
+
+	// Skip a line
+	cout << "" << endl;
+
+	// Counting
+	cout << "0" << endl;
+	cout << "1" << endl;
+	cout << "2" << endl;
+	cout << "3" << endl;
+	cout << "4" << endl;
+	cout << "5" << endl;
+	cout << "4" << endl;
+	cout << "3" << endl;
+	cout << "2" << endl;
+	cout << "1" << endl;
+	cout << "0" << endl;
+
+	// skips a line
+	cout << "" << endl;
+
+	// Pauses the system and waits for user input
+	system("pause");
+
+	// Skips a line
+	cout << "" << endl;
+
+	// SyntacticalErrors-A)
+	std::cout << "SyntacticalErrors-A)" << std::endl;
+	std::cout << "Howdy AIE" << std::endl;
+	std::cout << "It's a fine day today as any!" << std::endl;
+	std::cout << "I really wish I had some potatoes..." << std::endl;
+
+	// Skip a line
+	cout << "" << endl;
+
+	system("pause");
+
+	// Skip a line
+	cout << "" << endl;
+
+	// SyntacticalErrors-B)
+	std::cout << "SyntacticalErrors-B)" << std::endl;
+	std::cout << "Four score and seven years ago" << std::endl;
+	std::cout << "our fathers brought forth on this continent," << std::endl;
+	std::cout << "a new nation," << std::endl;
+	std::cout << "conceived in Libery, and dedicated to the proposition" << std::endl;
+	std::cout << "that all men are created equal." << std::endl;
+
+	std::cout << "Now we are engaged in a great civil war," << std::endl;
+	std::cout << "testing whether that nation, or any nation" << std::endl;
+	std::cout << "so conceived and so dedicated, can long endure." << std::endl;
+
+	// Skip a line
+	cout << "" << endl;
+
+	system("pause");
+
+	// Ends the program
+	return 0;
+}
